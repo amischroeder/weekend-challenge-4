fbf8ac488abb555b61583f093d2c63d644ebbb0c
Ami Schroeder <arschroeder22@gmail.com>
2017-08-19 10:36:19 -0500

    GET is working and added table to html

--- public/client.js
+++ public/client.js
@@ -1,1 +1,22 @@
 console.log('client.js has been loaded');
+
+var app = angular.module('EmployeeApp', []);
+
+app.controller('EmployeeController', ['$http', function ($http) {
+    console.log('Employee Controller has been loaded');
+    var self = this;
+    self.employees = [];
+
+    self.getEmployees = function () {
+        $http({
+            method: 'GET',
+            url: '/employee',
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.data);
+            self.employees = response.data;
+        }); //end of $http
+    }; //end of getEmployees
+
+    self.getEmployees();
+}]);
